e43d1fc45d1f2c7d96109c6d12e16c3c60a5e4ec
Christian Thompson <139803383+CThompson0101@users.noreply.github.com>
2023-11-17 09:17:58 -0600

    Add files via upload

--- webApplication.py
+++ webApplication.py
@@ -1,18 +1,15 @@
-import streamlit as st
-import tensorflow as tf
-from PIL import Image
-import numpy as np
+#import streamlit as st
+#import tensorflow as tf
+#from PIL import Image
+#import numpy as np
 import cv2
-import os
-
-os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 
 
 # Load the trained model
-def load_model():
-    model = tf.keras.models.load_model('model.hdf5')
-    return model
-model = load_model()
+#def load_model():
+#    model = tf.keras.models.load_model('model.hdf5')
+#    return model
+#model = load_model()
 
 # Add your logo and resize it
 logo_path = "logo.JPG"  # Replace with the path to your logo
@@ -46,74 +43,74 @@ def preprocess_image(image, target_size):
     return img_array
 
 # Check if an image is uploaded
-if uploaded_image is not None:
+#if uploaded_image is not None:
     # Display the uploaded image
-    st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+ #   st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
     # Perform image classification using your model
     # Preprocess the image (resize, normalize, etc.) as needed
-    img_array = preprocess_image(uploaded_image, target_size=(64, 64))
+  #  img_array = preprocess_image(uploaded_image, target_size=(64, 64))
     
     # Make predictions
-    predictions = model.predict(img_array)
+   # predictions = model.predict(img_array)
 
     # Get the class with the highest probability
-    predicted_class = np.argmax(predictions)
+    #predicted_class = np.argmax(predictions)
 
-    if (predicted_class == 1):
-        st.write(f"Diagnosis: Normal Skin Lesion/ Benign")
-    elif (predicted_class == 2):
-        st.write(f"Diagnosis: Benign Keratosis-like Lesion (BKL)")
-    elif (predicted_class == 3):
-        st.write(f"Diagnosis: Dermatofibroma (DF)")
-    elif (predicted_class == 4):
-        st.write(f"Diagnosis: Melanoma (MEL)")
-    elif (predicted_class == 5):
-        st.write(f"Diagnosis: Melanocytic Nevi (NV)")
-    elif (predicted_class == 6):
-        st.write(f"Diagnosis: Vascular Lesion (VASC)")
-    elif (predicted_class == 0):
-        st.write(f"Diagnosis: Actinic Keratoses (AKIEC)")
+    #if (predicted_class == 1):
+     #   st.write(f"Diagnosis: Normal Skin Lesion/ Benign")
+    #elif (predicted_class == 2):
+     #   st.write(f"Diagnosis: Benign Keratosis-like Lesion (BKL)")
+    #elif (predicted_class == 3):
+     #   st.write(f"Diagnosis: Dermatofibroma (DF)")
+    #elif (predicted_class == 4):
+     #   st.write(f"Diagnosis: Melanoma (MEL)")
+    #elif (predicted_class == 5):
+    #    st.write(f"Diagnosis: Melanocytic Nevi (NV)")
+    #elif (predicted_class == 6):
+    #    st.write(f"Diagnosis: Vascular Lesion (VASC)")
+    #elif (predicted_class == 0):
+    #    st.write(f"Diagnosis: Actinic Keratoses (AKIEC)")
 
-if capture_image:
+#if capture_image:
     # Add a delay before capturing the image (you can adjust the duration)
-    time.sleep(2)  # 2 seconds delay
+ #   time.sleep(2)  # 2 seconds delay
 
     # Use OpenCV to capture an image from the camera
-    cap = cv2.VideoCapture(0)  # 0 represents the default camera
-    ret, frame = cap.read()
-    cap.release()
+  #  cap = cv2.VideoCapture(0)  # 0 represents the default camera
+   # ret, frame = cap.read()
+    #cap.release()
 
     # Convert OpenCV frame to Pillow image
-    pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    #pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     # Display the captured image
-    st.image(pil_image, caption="Captured Image", use_column_width=True)
+    #st.image(pil_image, caption="Captured Image", use_column_width=True)
 
     # Perform image classification using your model
     # Preprocess the image (resize, normalize, etc.) as needed
-    img_array = preprocess_image(frame, target_size=(64, 64))
+    #img_array = preprocess_image(frame, target_size=(64, 64))
     
     # Make predictions
-    predictions = model.predict(img_array)
+    #predictions = model.predict(img_array)
 
     # Get the class with the highest probability
-    predicted_class = np.argmax(predictions)
+    #predicted_class = np.argmax(predictions)
 
-    if (predicted_class == 1):
-        st.write(f"Diagnosis: Normal Skin Lesion/ Benign")
-    elif (predicted_class == 2):
-        st.write(f"Diagnosis: Benign Keratosis-like Lesion (BKL)")
-    elif (predicted_class == 3):
-        st.write(f"Diagnosis: Dermatofibroma (DF)")
-    elif (predicted_class == 4):
-        st.write(f"Diagnosis: Melanoma (MEL)")
-    elif (predicted_class == 5):
-        st.write(f"Diagnosis: Melanocytic Nevi (NV)")
-    elif (predicted_class == 6):
-        st.write(f"Diagnosis: Vascular Lesion (VASC)")
-    elif (predicted_class == 0):
-        st.write(f"Diagnosis: Actinic Keratoses (AKIEC)")
+    #if (predicted_class == 1):
+     #   st.write(f"Diagnosis: Normal Skin Lesion/ Benign")
+    #elif (predicted_class == 2):
+     #   st.write(f"Diagnosis: Benign Keratosis-like Lesion (BKL)")
+    #elif (predicted_class == 3):
+     #   st.write(f"Diagnosis: Dermatofibroma (DF)")
+    #elif (predicted_class == 4):
+     #   st.write(f"Diagnosis: Melanoma (MEL)")
+    #elif (predicted_class == 5):
+     #   st.write(f"Diagnosis: Melanocytic Nevi (NV)")
+    #elif (predicted_class == 6):
+    #    st.write(f"Diagnosis: Vascular Lesion (VASC)")
+    #elif (predicted_class == 0):
+    #    st.write(f"Diagnosis: Actinic Keratoses (AKIEC)")
 
 # Add a footer with additional information
 st.text("This is a simple skin lesion classifier web app.")
